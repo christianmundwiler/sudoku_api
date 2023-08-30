1d9016d110698a6fe6055feba29d5dc8f8198beb
christianmundwiler <christian.mundwiler@ucdenver.edu>
2023-08-29 20:29:38 -0600

    added docker containerization

--- webapp/backend.py
+++ webapp/backend.py
@@ -1,15 +1,19 @@
-from flask import Flask, request
-from flask_cors import CORS, cross_origin
+from flask import Flask, request, render_template
+from flask_cors import CORS
 import requests
 import os
 
 from json import loads, dumps
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates/pages", static_folder="templates/static")
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/solve', methods=["POST"])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/solve", methods=["POST"])
 def solve():
     request_data = loads(request.get_data())
     print(request_data)
@@ -26,4 +30,4 @@ def solve():
     return response_result
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
